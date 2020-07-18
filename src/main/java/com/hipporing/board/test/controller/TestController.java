7e83d0f69903fb087e4d51be1f518a21914d023a
jighog1 <jighog1@gmail.com>
2020-07-18 17:08:29 +0900

    first commit

---
+++ src/main/java/com/hipporing/board/test/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.hipporing.board.test.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hipporing.board.test.service.TestService;
+import com.hipporing.board.test.vo.TestVO;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+	
+	@Autowired
+	private TestService testservice;
+	
+	@RequestMapping(value = "/home", method = {RequestMethod.GET})
+	public String home() {
+	      return "/test/home";
+	   }
+	
+	@RequestMapping(value = "/gettest/{key}", method = {RequestMethod.GET})
+	public TestVO testservice(@PathVariable(name = "key", required = true) int key) {
+		return testservice.getTest(key);
+	}
+	
+	@RequestMapping(value = "/gettests", method = {RequestMethod.GET})
+	public List<TestVO> testservices() {
+		return testservice.getTests();
+	}
+	
+}
